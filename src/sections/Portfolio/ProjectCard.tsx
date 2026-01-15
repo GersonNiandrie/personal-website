1b69aae383f9a74aa6008e589f1c570e0f94459a
GersonNiandrie <Gniandrie@gmail.com>
2026-01-15 16:00:36 +0700

    style add colour to project card shadow

--- src/sections/Portfolio/ProjectCard.tsx
+++ src/sections/Portfolio/ProjectCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ProjectCard({ project }: Props) {
   return (
-    <div className="card bg-base-100 shadow-sm group transition-all duration-300 hover:-translate-y-1 hover:shadow-lg-blue-500/50">
+    <div className="card bg-base-100 shadow-sm group transition-all duration-300 hover:-translate-y-1 hover:shadow-lg hover:bg-blue-500">
 
       {/* Image */}
       {project.image && (
